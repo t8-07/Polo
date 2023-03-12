b6ee90ea47ff4d8101ea810ac2af80940018159b
Andrei Verkeev <99138227+t8-07@users.noreply.github.com>
2023-03-12 12:24:21 +0300

    Polo Progect first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+let buttonClick = document.getElementById("buttonClick");
+
+buttonClick.addEventListener("click", onButtonClick);
+
+const genetationOne = [2009, 2010, 2011, 2012, 2013, 2014, 2015];
+const generationTwo = [2016, 2017, 2018, 2019];
+const generationThree = [2020, 2021, 2020];
+
+function onButtonClick() {
+    let inputOne = document.getElementById("whatGenIn");
+    let oneClick = Number(inputOne.value);
+
+    if (genetationOne.includes(oneClick)) {
+        window.alert("VW Polo Sedan 1 gen");
+    } else if (generationTwo.includes(oneClick)) {
+        window.alert("VW Polo Sedan 2 gen");
+    } else if (generationThree.includes(oneClick)) {
+        window.alert("VW Polo Sedan 3 gen");
+    }
+}
+
+
